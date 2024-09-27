916c5440f00c41aa0878f0395bb989aaaa73af3b
Qiantan Hong <qthong@stanford.edu>
2024-09-26 23:42:01 -0700

    fix add-mode-hook

--- configuration.lisp
+++ configuration.lisp
@@ -8,15 +8,21 @@
     :fn (lambda (object)
           (hooks:add-hook
            (enable-hook object)
-           (lambda (mode)
-             (enable-modes* toggle-mode (buffer mode))))
+           (make-instance
+            'hooks:handler
+            :name toggle-mode
+            :fn (lambda (mode)
+                  (enable-modes* toggle-mode (buffer mode)))))
           (hooks:add-hook
            (disable-hook object)
-           (lambda (mode)
-             (disable-modes* toggle-mode (buffer mode)))))))
+           (make-instance
+            'hooks:handler
+            :name toggle-mode
+            :fn (lambda (mode)
+                  (disable-modes* toggle-mode (buffer mode))))))))
   toggle-mode)
 
-(defun remove-move-hook (trigger-mode toggle-mode)
+(defun remove-mode-hook (trigger-mode toggle-mode)
   (hooks:remove-hook
    (nyxt::customize-hook (find-class trigger-mode))
    toggle-mode)
