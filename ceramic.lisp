4c3027df46f4e3b3dcd1526b86c981ba341e75b7
Qiantan Hong <qthong@stanford.edu>
2024-11-07 15:40:49 +0800

    revert ceramic to use cond var
    The deadlock mailbox "fixed" was actually caused by Electron not
    resolving executeJavaScript promises after webContents.destroy(). That
    is now fixed by a timeout.

--- ceramic.lisp
+++ ceramic.lisp
@@ -1,12 +1,17 @@
 (in-package #:neomacs)
 
+(defun quote-js (js-code)
+  "Replace each backslash with 2 (unless a \" follows it) and escape backquotes."
+  (ppcre:regex-replace-all "`"
+                           (ppcre:regex-replace-all "\\\\(?!\")" js-code "\\\\\\\\")
+                           "\\\\`"))
+
 (defvar *force-sync-evaluate* nil)
 
 (defun send-js-for-buffer (code buffer)
   (cera.d:js
    cera.d:*driver*
-   (ps:ps (ps:chain (js-buffer buffer) web-contents
-                    (execute-java-script (ps:lisp code) t)))))
+   (format nil "Ceramic.buffers[~S].webContents.executeJavaScript(`~A`,true)" (id buffer) (quote-js code))))
 
 (sera:-> evaluate-javascript (string (or buffer (eql :global))) null)
 (defun evaluate-javascript (code buffer)
@@ -27,47 +32,21 @@ BUFFER is :global. Returns NIL."
          (cera.d:js cera.d:*driver* code))))
   nil)
 
-
 (sera:-> evaluate-javascript-sync (string (or buffer (eql :global))) t)
 (defun evaluate-javascript-sync (code buffer)
   "Evaluate JavaScript CODE synchronously and return the result.
 
 Evaluate CODE in BUFFER's webContents, or main Electron process if
 BUFFER is NIL."
-  (let* ((message-id
-           (uuid:format-as-urn nil (uuid:make-v4-uuid)))
-         (full-js
-           (etypecase buffer
-             (buffer
-              (ps:ps
-                (ps:chain
-                 -ceramic
-                 (sync-eval
-                  (ps:lisp message-id)
-                  (lambda ()
-                    (ps:chain (js-buffer buffer) web-contents
-                              (execute-java-script (ps:lisp code) t)))))))
-             ((eql :global)
-              (ps:ps
-                (ps:chain
-                 -ceramic
-                 (sync-eval
-                  (ps:lisp message-id)
-                  (lambda () (eval (ps:lisp code))))))))))
-    (with-slots (threads responses cera.d::js-lock)
-        cera.d:*driver*
-      (let ((mailbox
-              (bt:with-lock-held (cera.d::js-lock)
-                (setf (gethash message-id responses)
-                      (or (gethash (bt:current-thread) threads)
-                          (setf (gethash (bt:current-thread) threads)
-                                (sb-concurrency:make-mailbox)))))))
-        (cera.d:js cera.d:*driver* full-js)
-        (sb-concurrency:receive-message mailbox)))))
+  (cera.d:sync-js
+   cera.d:*driver*
+   (etypecase buffer
+     (buffer
+      (format nil "return Ceramic.buffers[~S].webContents.executeJavaScript(`~A`,true)" (id buffer) (quote-js code)))
+     ((eql :global)
+      (format nil "return eval(`~A`)" (quote-js code))))))
 
-(defclass driver (ceramic.driver:driver)
-  ((responses :initform (make-hash-table :test 'equal))
-   (threads :initform (make-hash-table :weakness :key))))
+(defclass driver (ceramic.driver:driver) ())
 
 (setq cera.d:*driver* (make-instance 'driver)
       trivial-ws:+default-timeout+ 1000000
@@ -77,13 +56,12 @@ BUFFER is NIL."
 (defmethod ceramic.driver::on-message ((driver driver) message)
   (declare (type string message))
   (let ((data (cl-json:decode-json-from-string message)))
-    (with-slots (responses cera.d::js-lock) driver
+    (with-slots (cera.d::responses cera.d::js-cond cera.d::js-lock) driver
       (if-let (id (assoc-value data :id))
         (bt:with-lock-held (cera.d::js-lock)
-          (sb-concurrency:send-message
-           (gethash id responses)
-           (assoc-value data :result))
-          (remhash id responses))
+          (setf (gethash id cera.d::responses)
+                (assoc-value data :result))
+          (bt2:condition-broadcast cera.d::js-cond))
         (sb-concurrency:send-message *event-queue* data)))))
 
 (define-command kill-neomacs ()
