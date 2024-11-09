0982d4c60535afaa64dd43cf1f5a194971914ef3
Qiantan Hong <qthong@stanford.edu>
2024-11-09 14:53:55 +0800

    support config file

--- start.lisp
+++ start.lisp
@@ -1,6 +1,12 @@
 (in-package #:neomacs)
 
 (defun start (&optional (use-neomacs-debugger t))
+  (let ((config-file (uiop:xdg-config-home "neomacs" "init.lisp")))
+    (if (uiop:file-exists-p config-file)
+        (progn
+          (format t "Loading ~a.~%" config-file)
+          (load config-file))
+        (format t "~a not yet exist.~%" config-file)))
   (load-web-history)
   (unless ceramic.runtime:*releasep*
     (ceramic:setup))
